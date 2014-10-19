1592dfe9e9b18360e0fa3558fd44271cc4f76a85
Adrian Sampson <adrian@radbox.org>
2014-10-18 18:04:31 -0700

    Start to replace epdata.liquid
    Ruby may be silly, but it's not as bad as Liquid.

--- _plugins/jekyllcast.rb
+++ _plugins/jekyllcast.rb
@@ -14,8 +14,20 @@ module Jekyll
       col = Collection.new(site, "eps")
       site.collections["ep_players"] = col
 
-      if site.data["podcast"]["player_card"]
-        site.collections["eps"].docs.each do |ep|
+      sc = site.config
+      pc = site.data["podcast"]
+      surl = sc['url']
+
+      site.collections["eps"].docs.each do |ep|
+        ep.data.merge!({
+          "imageurl" => ep.data['image'] ?
+                        "#{surl}#{ep.data['image']}" :
+                        "#{surl}#{pc['image']}",
+          "ogg" => "#{surl}audio/#{ep.data['number']}.ogg",
+          "mp3" => "#{surl}audio/#{ep.data['number']}.mp3",
+        })
+
+        if site.data["podcast"]["player_card"]
           player = PlayerDocument.new(ep.path, { site: site, collection: col })
           player.read
           player.data["layout"] = "player"
